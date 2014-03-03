4985bb82c526fbfae47a3a09d97026ea88b75ae0
Jacob Mick <anon.biohacker@gmail.com>
2014-03-02 20:34:35 -0600

    sassaprilla layout

--- Views/Home.hs
+++ Views/Home.hs
@@ -20,22 +20,22 @@ import           Web.Scotty                  (ActionM)
 
 layout :: Html -> Html -> Html
 layout t b = docTypeHtml $ do
-           pet "<!--[if lt IE 7]>      <html class='no-js lt-ie9 lt-ie8 lt-ie7'> <![endif]-->"
-           pet "<!--[if IE 7]>         <html class='no-js lt-ie9 lt-ie8'/> <![endif]-->"
-           pet "<!--[if IE 8]>         <html class='no-js lt-ie9'> <![endif]-->"
-           pet "<!--[if gt IE 8]><!--> <html class='no-js'> <!--<![endif]-->"
+           pet "<!--[if lt IE 9 ]> <html lang="en-GB" class="no-js lt-ie9"> <![endif]-->"
+           pet "<!--[if IE 9 ]> <html lang="en-GB" class="no-js ie9"> <![endif]-->"
+           pet "<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en-GB" class="no-js"> <!--<![endif]-->"
            head $ do
              title t
              meta ! charset "utf-8"
              meta ! httpEquiv "X-UA-Compatible" ! content "IE=edge,chrome=1"
+             link ! href "assets/css/screen.min.css" ! rel "stylesheet"
+             link ! rel "shortcut icon" ! href "favicon.ico"
              meta ! name "description" ! content "Inspire Text"
-             meta ! name "viewport" ! content "width=device-width"
-             link ! href "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" ! rel  "stylesheet" ! media "screen"
+             meta ! name "viewport" ! content "initial-scale=1"
              style $ pet $ toStrict layoutCss
            body $ do
              navBar >> b
              script ! src "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" $ mempty
-             script ! src "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js" $ mempty
+             script ! src "assets/js/script.min.js" $ mempty
 
 homeView :: ActionM ()
 homeView = blaze $ layout "home" $ do
